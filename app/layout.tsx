aece8a008c90031d7a26b43698228bb1302853db
UmenSama <umensamare@gmail.com>
2025-07-23 09:57:51 +0530

    Build auth using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,11 +27,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar/>
-        {children}
+    
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+          <ClerkProvider appearance = {{variables:{
+            colorPrimary: '#fe5933'}}}> { /* wrapped our application around the lerk Provider*/ }
+            <Navbar />
+            {children}
+          </ClerkProvider>
+          
         </body>
-    </html>
+      </html>
   );
 }
